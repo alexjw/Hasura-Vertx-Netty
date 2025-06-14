e543a343804e92d00d49b639edf92fef4aea4789
Alex Jines <abel.oalex@gmail.com>
2025-06-14 16:37:59 -0300

    added the idea for the new Battle start

--- src/main/java/org/example/hasuravertxnetty/controller/BattleController.java
+++ src/main/java/org/example/hasuravertxnetty/controller/BattleController.java
@@ -1,27 +1,24 @@
 package org.example.hasuravertxnetty.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import org.example.hasuravertxnetty.models.Battle;
 import org.example.hasuravertxnetty.models.BattleParticipant;
 import org.example.hasuravertxnetty.models.Player;
-import org.example.hasuravertxnetty.repositories.PlayerRepository;
 import org.example.hasuravertxnetty.services.BattleService;
-import org.example.hasuravertxnetty.services.HasuraService;
 import org.example.hasuravertxnetty.services.PlayerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
@@ -59,7 +57,56 @@ public class BattleController {
         this.battleService = battleService;
     }
 
+
+
     @PostMapping("/start")
+    public String startBattle2(@RequestBody Map<String, Object> input) throws InterruptedException {
+        int matchSize = 32;
+        logger.info("Starting battle simulation with {} players", matchSize);
+        Integer id = (Integer) ((Map<String, Object>)((Map<String, Object>)((Map<String, Object>) input.get("event")).get("data")).get("new")).get("id");
+        List<Player> players = new ArrayList<>();
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // Like ServerSocket
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) {
+                            ch.pipeline().addLast(new StringEncoder());
+                            System.out.println("Received: " + ch.toString());
+                            ch.writeAndFlush("Hello"); // Non-blocking
+                        }
+                    });
+            bootstrap.bind(8089).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+        Battle theBattle = battleService.findBattleById(id);
+        theBattle.setStartTime(LocalDateTime.now());
+
+        for (int i = 0; i < players.size(); i++) {
+            BattleParticipant battleParticipant = new BattleParticipant();
+            battleParticipant.setBattle(theBattle);
+            battleParticipant.setPlayer(players.get(i));
+            battleParticipant.setTeam(i % 2 == 0 ? "allies" : "axis");
+            battleParticipant.setScore((int) Math.round(Math.random() * 1000));
+            battleService.saveBattleParticipant(battleParticipant);
+        }
+
+        logger.info("Battle simulation started");
+        long duration = Math.round(Math.random() * 10000);
+        Thread.sleep(duration);
+        players.forEach(player -> player.setLookingForBattle(true));
+        playerService.saveAll(players);
+        logger.info("Battle simulation completed in {} ms", duration);
+        return "Battle simulation completed";
+    }
     public String startBattle(@RequestBody Map<String, Object> input) throws InterruptedException {
         int matchSize = 32;
         logger.info("Starting battle simulation with {} players", matchSize);
@@ -86,7 +133,7 @@ public class BattleController {
             new Thread(() -> {
                 EventLoopGroup group = new NioEventLoopGroup();
                 try {
-                    Thread.sleep(Math.round(Math.random() * 10000));
+                    //Thread.sleep(Math.round(Math.random() * 10000));
                     Bootstrap client = new Bootstrap()
                             .group(group)
                             .channel(NioSocketChannel.class)
